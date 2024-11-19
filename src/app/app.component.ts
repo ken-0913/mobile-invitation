61b1e250494f323c2c57f768ca16bdf6acf50a96
HYEONJAE LEE <hyeonjaelee@HYEONJAEs-MacBook-Air.local>
2024-11-20 00:42:32 +0900

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FirstMainComponent} from './first-main/first-main.component';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {WeddingCalanderComponent} from './wedding-calander/wedding-calander.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstMainComponent, MatGridList, MatGridTile, WeddingCalanderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mobile-invitation-v1';
