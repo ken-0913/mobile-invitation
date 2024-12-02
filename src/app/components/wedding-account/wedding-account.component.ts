9937558950c123590e17a45a712f31f4f7f90949
HYEONJAE LEE <hyeonjaelee@HYEONJAEs-MacBook-Air.local>
2024-12-03 00:06:03 +0900

    fix

--- src/app/components/wedding-account/wedding-account.component.ts
+++ src/app/components/wedding-account/wedding-account.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, input, signal} from '@angular/core';
 import {MatCard, MatCardContent} from '@angular/material/card';
 import {MatFormField} from '@angular/material/form-field';
 import {MatOption, MatSelect} from '@angular/material/select';
+import {CdkCopyToClipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-wedding-account',
@@ -11,32 +12,16 @@ import {MatOption, MatSelect} from '@angular/material/select';
     MatCard,
     MatFormField,
     MatSelect,
-    MatOption
+    MatOption,
+    CdkCopyToClipboard
   ],
   templateUrl: './wedding-account.component.html',
   styleUrl: './wedding-account.component.scss'
 })
-export class WeddingAccountComponent implements AfterViewInit {
-  private inputElement: HTMLInputElement | undefined;
-
-  ngAfterViewInit(): void {
-    this.inputElement = document.querySelector('.custom-input') as HTMLInputElement;
-  }
-
-  async copyToClipboard(): Promise<void> {
-
-    const inputElement = document.querySelector('.customer-input') as HTMLInputElement;
-    if (inputElement) {
-      try {
-        // Clipboard API를 사용하여 클립보드에 복사
-        await navigator.clipboard.writeText(inputElement.value);
-        alert('계좌번호가 복사되었습니다!');
-      } catch (err) {
-        console.error('클립보드 복사 실패', err);
-        alert('클립보드 복사에 실패했습니다.');
-      }
-    } else {
-      alert('입력 요소를 찾을 수 없습니다.');
-    }
+export class WeddingAccountComponent{
+  brideAccount = '1234-1234-1234';
+  groomAccount = '4321-4321-4321';
+  showCopiedAlert(): void {
+    alert('계좌번호가 복사 되었습니다. ')
   }
 }
